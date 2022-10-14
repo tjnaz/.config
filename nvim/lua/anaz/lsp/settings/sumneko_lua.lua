dad8122b99bacb8f330e40bfe409912f27aced40
Thaj Anaz <anaz.thaj@gmail.com>
2022-10-14 22:02:40 +0530

    Try some solution for global vim undefined

--- nvim/lua/anaz/lsp/settings/sumneko_lua.lua
+++ nvim/lua/anaz/lsp/settings/sumneko_lua.lua
@@ -3,14 +3,9 @@ return {
 
 		Lua = {
 			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.stdpath("config") .. "/lua"] = true,
-				},
+				globals = { 'vim' },
 			},
 		},
 	},
 }
+
