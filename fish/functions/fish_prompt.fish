19cbcfe74ce0371285017fa04275ff7e801c56fd
Thaj Anaz <anaz.thaj@gmail.com>
2022-10-30 14:07:43 +0530

    Configure prompt for git icons

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -35,7 +35,7 @@ function fish_prompt --description 'Write out the prompt'
         set -g __fish_git_prompt_char_invalidstate "✖ -"
     end
     if not set -q __fish_git_prompt_char_cleanstate
-        set -g __fish_git_prompt_char_cleanstate "✔-"
+        set -g __fish_git_prompt_char_cleanstate "✔"
     end
     if not set -q __fish_git_prompt_color_dirtystate
         set -g __fish_git_prompt_color_dirtystate blue
