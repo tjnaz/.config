ed3a9b5ce772b73bdde891fb30265828b8aa600e
Thaj Anaz <anaz.thaj@gmail.com>
2022-11-11 22:46:19 +0530

    Test some old .config file functionalities

--- nvim-old/after/plugin/mason.rc.lua
+++ nvim-old/after/plugin/mason.rc.lua
@@ -1,31 +1,31 @@
 local status, mason = pcall(require, "mason")
 if not status then
-	return
+  return
 end
 
 local status2, lspconfig = pcall(require, "mason-lspconfig")
 if not status2 then
-	return
+  return
 end
 
 mason.setup({})
 lspconfig.setup({
-	ensure_installed = { "tailwindcss", "emmet-ls", "stylelua", "sumneko_lua" },
+  ensure_installed = { "tailwindcss", "emmet-ls", "stylelua", "sumneko_lua" },
 })
 
 require("lspconfig").tailwindcss.setup({})
 require("lspconfig").emmet_ls.setup({
-	-- on_attach = on_attach,
-	capabilities = capabilities,
-	filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" },
-	init_options = {
-		html = {
-			options = {
-				-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-				["bem.enabled"] = true,
-			},
-		},
-	},
+  -- on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less" },
+  init_options = {
+    html = {
+      options = {
+        -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+        ["bem.enabled"] = true,
+      },
+    },
+  },
 })
 
 require("lspconfig").sumneko_lua.setup({})
