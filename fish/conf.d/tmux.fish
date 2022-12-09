78e5480cd1d0f76814af6aa7fdef02324f47e87b
Thaj Anaz <anaz.thaj@gmail.com>
2022-12-09 20:12:41 +0530

    Getting rid of some stuff

--- fish/conf.d/tmux.fish
+++
@@ -1,8 +0,0 @@
-if not set -q TMUX
-#     set -g TMUX tmux new-session -d -s trial
-#     eval $TMUX
-#     tmux attach-session -d -t trial
-  # tmux -L trial -f ~/.tmux-alacritty.conf new-session -d -s trial
-tmux -L "m4st3rm!nd" -f ~/.tmux-alacritty.conf new-session -d -s "m4st3rm!nd"
-  tmux -L "m4st3rm!nd" attach -t "m4st3rm!nd"
-end
